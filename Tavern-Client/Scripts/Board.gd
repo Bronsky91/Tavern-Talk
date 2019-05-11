cf65edde71f1796b08d35b0d4d6881aa074ffaa6
Bryan Reed <bryan87reed@gmail.com>
2019-05-11 11:10:25 -0700

    Implements bulletin board, still needs updates with edits

--- Tavern-Client/Scripts/Board.gd
+++ Tavern-Client/Scripts/Board.gd
@@ -1,36 +1,57 @@
 extends Node2D
 
-export(PackedScene) var post
-
-var post_list = []
+export(PackedScene) var post_scene
+onready var post_buttons = [
+ $Board/TextureButton1, $Board/TextureButton2, $Board/TextureButton3, $Board/TextureButton4,
+ $Board/TextureButton5, $Board/TextureButton6, $Board/TextureButton7, $Board/TextureButton8
+]
+var post_dict = {1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null}
 
 func _ready():
-	$BoardRequest.request(global.api_url + 'tavern/' + global.player_data.tavern.id + '/board')
+	## TEMP FOR DEBUG
+	global.player_data.tavern.id = "5cbf609bcfa4e90275f81e4c"
+	###
+	$Board/BoardRequest.request(global.api_url + 'tavern/' + global.player_data.tavern.id + '/board')
 	
 func populate_posts(posts):
-	# Only populate posts that aren't already on the board
-	for post in post_list:
-		for p in posts:
-			if post._id == p._id:
-				posts.erase(p)
-	# Instance all remaining posts
-	for p in posts:
-		var new_post = post.instance()
-		new_post.new_post(false, p._id, p.body, p.author)
-		post_list.append(new_post)
-		$GridContainer.add_child(post)
-		## TODO: Adjust size of each post or use placeholder image
-		## Maybe use placeholder and attach the post ID to each placeholder?
+	## Populates post_dic with the _ids of all posts
+	var post_num = 0
+	for post in posts:
+		post_dict[post_num] = post
+		post_num = post_num + 1
 
-func create_post():
-	var new_post = post.instance()
-	new_post.new_post(true)
-	## Change scene to post scene?
+	for key in post_dict:
+		if post_dict[key] != null: ## if the post isn't null, populate it on the board
+			print(post_buttons)
+			post_buttons[key].disabled = false
+			post_buttons[key].visible = true
+
 	
 func _on_BoardRequest_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
+	print(json.result)
 	var posts = json.result.data
 	populate_posts(posts)
 
 func _on_NewPost_button_up():
-	get_tree().change_scene("Scenes/Post.tscn")
+	var new_post = post_scene.instance()
+	new_post.new_post(true)
+	$Board.hide()
+	add_child(new_post)
+	
+func _on_BackButton_button_up():
+	get_tree().change_scene("Scenes/Tavern.tscn")
+
+func _on_TextureButton_button_up(num):
+	var post_data = post_dict[num]
+	var new_post = post_scene.instance()
+	add_child(new_post)
+	new_post.new_post(false, post_data._id, post_data.body, post_data.author)
+	$Board.hide()
+	
+func _on_Board_visibility_changed():
+	## TEMP FOR DEBUG
+	global.player_data.tavern.id = "5cbf609bcfa4e90275f81e4c"
+	###
+	if $Board.visible == true:
+		$Board/BoardRequest.request(global.api_url + 'tavern/' + global.player_data.tavern.id + '/board')
--- Tavern-Client/Scripts/Post.gd
+++ Tavern-Client/Scripts/Post.gd
@@ -8,6 +8,7 @@ var post_limit = 359
 var post_id = '0'
 
 func _ready():
+	## May need to change to _enter_tree
 	if not new_post:
 		$Limit.text = "Character Limit " + str($BodyEdit.text.length()) + "/358"
 		$NewEdit.text = "Edit"
@@ -16,7 +17,7 @@ func _input(event):
 	if Input.is_action_just_pressed('backspace') and $BodyEdit.cursor_get_line() > 0:
 		$BodyEdit.readonly = false
 	
-func new_post(new, id=null, body=null, author=null):
+func new_post(new, id, body, author):
 	## Called from Board before post scene is added to scene_tree
 	if new:
 		new_post = true
@@ -27,8 +28,9 @@ func new_post(new, id=null, body=null, author=null):
 		write_post(body, author)
 
 func write_post(body, author):
+	print(post_body)
 	post_body.text = body
-	post_author = "- " + author
+	post_author.text = author
 	
 func _on_TextEdit_text_changed():
 	$Limit.text = "Character Limit " + str($BodyEdit.text.length()) + "/358"
@@ -43,9 +45,11 @@ func _on_NewEdit_button_up():
 		hide_edit(true)
 		new_post = false
 		$NewEdit.text = 'Edit'
-		# var post_data = {id: post_id, body: $BodyEdit.text, author: $AuthorEdit.text}
+		var post_data = {'id': post_id, 'body': $BodyEdit.text, 'author': $AuthorEdit.text}
 		### TODO: Add logic to API that if id == '0', it's a new post otherwise edit it properly
-		# global.make_post_request($PostSave, 'save_post', post_data, false)
+		print('tavern/' + global.player_data.tavern.id + '/board')
+		print(post_data)
+		global.make_post_request($PostSave, 'tavern/' + global.player_data.tavern.id + '/board', post_data, false)
 	else:
 		hide_edit(false)
 		$NewEdit.text = 'Post'
@@ -57,13 +61,17 @@ func hide_edit(hide):
 		$AuthorEdit.hide()
 	else:
 		$BodyEdit.show()
+		$BodyEdit.text = post_body.text
 		$AuthorEdit.show()
+		$AuthorEdit.text = post_author.text
 	
 func _on_Return_button_up():
-	get_tree().change_scene("Scenes/Board.tscn")
+	get_parent().find_node("Board").show()
+	queue_free()
 	
 func _on_PostSave_request_completed(result, response_code, headers, body):
-	pass # Replace with function body.
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(json.result)
 	## Display error if post did not save for some reason
 
 
