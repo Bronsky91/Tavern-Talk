006c78edf6b77a1fdaf18450f28474a1e6301944
Bryan Reed <bryan87reed@gmail.com>
2019-05-18 06:28:12 -0700

    Fixes the tavern script to accommodate new art
    Removes the table refresh and the table count labels. Will repurpose the API table character counter at a later time

--- Tavern-Client/Scripts/Tavern.gd
+++ Tavern-Client/Scripts/Tavern.gd
@@ -3,8 +3,6 @@ extends Node2D
 export(PackedScene) var table
 
 var character_name = null
-var number_of_tables = null
-var setup_run = true
 
 func _ready():
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
@@ -12,6 +10,7 @@ func _ready():
 	if global.player_data.tavern.ip != null and global.player_data.tavern.port != null and global.player_data.table_id == 0:
 		character_name = global.player_data.character.name
 		enter_tavern(global.player_data.tavern.ip, global.player_data.tavern.port)
+		create_table_scenes()
 
 func enter_tavern(ip, port):
 	var host = NetworkedMultiplayerENet.new()
@@ -20,7 +19,6 @@ func enter_tavern(ip, port):
 
 func entered_tavern():
 	print('entered')
-	get_table_stats()
 
 func leave_tavern():
 	get_tree().set_network_peer(null)
@@ -37,39 +35,17 @@ func join_table(table_id):
 	for t in get_tree().get_nodes_in_group("tables"):
 		if t.table_id == table_id:
 			t.show()
-
-func get_table_stats():
-	# makes HTTPRequest to refresh the character count for each table
-	var headers = ["Content-Type: application/json"]
-	$RefreshCountTimer/TableRefresh.request(global.api_url + 'tavern/' + global.player_data.tavern.id + '/tables')
 	
-func _on_RefreshCountTimer_timeout():
-	get_table_stats()
-
-func find_table(table_id):
-	var tables = {1: $Count1, 2: $Count2, 3: $Count3, 4: $Count4}
-	return tables[table_id]
-
-func _on_TableRefresh_request_completed(result, response_code, headers, body):
-	var n_tables = 0 # number of tables on this request
-	var json = JSON.parse(body.get_string_from_utf8())
-	for table_id in json.result.data.keys():
-		n_tables = n_tables + 1
-		find_table(int(table_id)).text = "Player Count: " + str(json.result.data[table_id])
-	number_of_tables = n_tables # Updates the total number of tables in the tavern
-	create_table_nodes()
-
-func create_table_nodes():
-	if setup_run:
-		setup_run = false
-		for t in range(1, number_of_tables + 1):
-			# instance packed scene
-			var new_table = table.instance()
-			new_table.assign(t)
-			new_table.set_name("Table" + str(t))
-			new_table.hide()
-			add_child(new_table)
-			new_table.add_to_group("tables")
+func create_table_scenes():
+	for t in range(1, 4):
+		# instance packed scene
+		var new_table = table.instance()
+		new_table.assign(t)
+		new_table.set_name("Table" + str(t))
+		new_table.hide()
+		new_table.z_index = 4
+		add_child(new_table)
+		new_table.add_to_group("tables")
 
 func _on_Board_button_up():
 		get_tree().change_scene("Scenes/Board.tscn")
