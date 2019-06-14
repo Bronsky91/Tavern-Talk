cbe54cff23367bf76d42280d81844565dcbb9f25
Bryan <bryan87reed@gmail.com>
2019-06-14 09:12:11 -0700

    Auto stash before revert of "Barrel game started"

--- Tavern-Client/Scripts/BarrelRoll.gd
+++ Tavern-Client/Scripts/BarrelRoll.gd
@@ -5,10 +5,11 @@ export(PackedScene) var barrel_scene
 onready var barrel = $Paths/Right/PathFollow2D
 
 var start = false
+var b_speed = 100
 
 func _ready():
-	$Paths/Right/PathFollow2D/Barrel.playing = true
-	$Paths/Middle/PathFollow2D/Barrel.playing = true
+	$Paths/Right/PathFollow2D/Barrel.play("Down")
+	$Paths/Middle/PathFollow2D/Barrel.play("Down")
 
 func _process(delta):
 	if start:
@@ -17,3 +18,15 @@ func _process(delta):
 
 func _on_Start_button_up():
 	start = true
+
+func _on_Area2D_area_entered(area):
+	b_speed = 0
+	$Paths/Middle/PathFollow2D/Barrel.z_index = 1
+	$Paths/Middle/PathFollow2D/Barrel.play("HillDown")
+	start = false
+
+func _on_Barrel_animation_finished():
+	if $Paths/Middle/PathFollow2D/Barrel.animation == "HillDown":
+		$Paths/Middle/PathFollow2D/Barrel.play("Down")
+		start = true
+		b_speed = 100
